c1588cd6fb3f8f66856ef27095eebceba1028bfd
EmFoley <foleyemilym@gmail.com>
2014-12-13 18:43:59 -0800

    radio buttons

--- piglatin/app/assets/javascripts/pairs.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-	var $picker = $(".translate-picker")
-
-});​
--- piglatin/app/controllers/pages_controller.rb
+++ piglatin/app/controllers/pages_controller.rb
@@ -1,6 +1,22 @@
 class PagesController < ApplicationController
 
 	def index
+		@pairs = Pair.all
+	end
+
+	def new
+		@pair = Pair.new()
+	end
+
+	def create
+		@pair = Pair.new(params[:pair])
+		if @pair.save
+			render partial: 'translations' 
+		end
+	end
+
+	def show
+		@pair = Pair.find(params[:key])
 	end
 	
 end
