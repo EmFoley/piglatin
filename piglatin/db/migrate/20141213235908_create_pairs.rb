c47f77bbb768b2233b636967619656e929c3ca10
EmFoley <foleyemilym@gmail.com>
2014-12-13 16:03:27 -0800

    add pairs table

---
+++ piglatin/db/migrate/20141213235908_create_pairs.rb
@@ -0,0 +1,6 @@
+class CreatePairs < ActiveRecord::Migration
+  def change
+    create_table :pairs do |t|
+    end
+  end
+end
--- piglatin/db/schema.rb
+++ piglatin/db/schema.rb
@@ -11,9 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141213235908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pairs", force: true do |t|
+  end
 
 end
